7d266432879c6fe3fd2986d14be2f17e5ea38a11
Connor-Lemons <c25connor.emmons@afacademy.af.edu>
2024-08-23 09:04:31 -0600

    Added Newton's method code

---
+++ findZero.m
@@ -0,0 +1,20 @@
+function [p, i] = findZero(f, p_0, TOL)
+    
+    syms x
+    f_prime = diff(f, x);
+    
+    for i = 1:1000
+    
+        p = p_0 - f(p_0)/f_prime(p_0);
+    
+        if abs(p - p_0) < TOL
+            break
+        end
+    
+        p_0 = p;
+    
+    end
+
+    p = vpa(p);
+
+end
